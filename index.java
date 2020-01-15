36559377fa29364eb6bb8253bb368d76c0fff8a4
mel63254 <55901724+mel63254@users.noreply.github.com>
2020-01-14 23:41:52 -0800

    Create index.java

---
+++ index.java
@@ -0,0 +1,39 @@
+import static java.lang.Math.*;
+public class Lecture3
+ {
+      public static void main(String[] args)
+ {
+     int x = 0, y = 0, z = 0;
+     double result;
+     final boolean isOdd;
+     int x, y, z;
+     x =(4*5)+3;
+     result =0.0;
+     /**The first bullet I'll call 5.1 so we understand which bullet I'm talking about,
+      *the second bullet = 5.2, and so on. Now 5.1 is above. For 5.2 the answer would
+      *be yes. For 5.3 the answer is No. For 5.4, this is the answer.
+      */
+      double x = 25;
+      double y = Math.sqrt(5);
+      z = 32%11;
+ }
+      {
+         System.out.println("The value of x is (x), the value of y is (y), the value of z is" + (32 % 11));
+        /* The value of x is 0, the value of y is 5, the value of z is 2.909090909 */
+        System.out.println("y is an odd number:",(isOdd));
+      }
+        /* The value of y is 5 */
+        {
+            System.out.println("The value of x is: (x)");
+        int x = 5;
+        /* X is 5*/
+}
+    {
+        double y = Math.pow(y, a);
+        System.out.println(y); // prints square root of y
+    }
+    {double x = 4;
+    double y = Math.sqrt(x);
+    System.out.println(y); //
+}
+     }
